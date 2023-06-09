cf73096c146452248680a3ff54b471828811cc4a
Dzemil <dzemilpapic5@gmail.com>
2023-06-09 15:32:10 +0200

    5th exercise done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(sumStart, sumEnd) {
+    if ((sumStart < 0) || (sumEnd < 0)) {
+        return "ERROR";
+    }  
+    else if ((typeof sumStart === "number") && (typeof sumEnd === "number")) {
+        const high = Math.max(sumStart, sumEnd);
+        const low = Math.min(sumStart, sumEnd);
+        
+        return high * (high + 1) / 2 - (low - 1) * (low) / 2;
+    }
+    else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
